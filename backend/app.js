388af2c5d6ede1e9d74f9209f04e5376c5e26aee
rohitsamal.mvteams <rohitsamal.mvteams@gmail.com>
2022-12-21 13:11:24 +0530

    added router paths

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dotenv = require('dotenv');
+const mysql = require('mysql');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+const variantsRouter = require('./routes/variants');
 
-var app = express();
+const app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,9 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(mysql);
+// app.use(dotenv);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/variants', variantsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
