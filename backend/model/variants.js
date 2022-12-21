af9b50b4b81a539f97169cf33de2d30bb680bee6
rohitsamal.mvteams <rohitsamal.mvteams@gmail.com>
2022-12-21 13:09:42 +0530

    added database config and define models for tables

---
+++ backend/model/variants.js
@@ -0,0 +1,11 @@
+const bookshelf = require('./dbconfig');
+
+const Variants = bookshelf.Model.extend({
+    tableName : "variants",
+
+    products : function () {
+        return this.belongsTo('Products', 'products_id', 'id')
+    }
+})
+
+module.exports = bookshelf.model("Variants", Variants);
