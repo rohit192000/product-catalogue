3e386ac1fda3f5f4c56cfa3f94893579821341fc
rohitsamal.mvteams <rohitsamal.mvteams@gmail.com>
2022-12-22 17:15:34 +0530

    removed multer

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,15 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mysql = require('mysql');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 
+const corsOption = {
+  origin : 'http://localhost:3000',
+  credentials : true,
+  optionSuccessStatus : 200
+}
 const productsRouter = require('./routes/products');
 const variantsRouter = require('./routes/variants');
 
@@ -18,8 +24,9 @@ app.set('view engine', 'ejs');
 
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+app.use(express.json({limit : '100mb'}));
+app.use(express.urlencoded({ extended: false, limit : '100mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(mysql);
